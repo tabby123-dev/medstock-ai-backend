7a2fc042332211e9f94283d53939db7c67b351b9
Tabitha Ndungu <85933175+tabby123-dev@users.noreply.github.com>
2026-02-22 16:38:51 +0300

    Fix formatting of app.listen call

--- src/main.ts
+++ src/main.ts
@@ -40,7 +40,7 @@ async function bootstrap() {
   app.useGlobalFilters(new HttpExceptionFilter());
 
   const port = process.env.PORT ?? 3000;
-  await app.listen(port, '0.0.0.0');
+  await app.listen( port, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}/${apiPrefix}`);
 }
 bootstrap();
