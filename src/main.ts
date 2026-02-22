b35fd4a1de5a8c9091ffedfbf5044b3212e8c63e
Tabitha Ndungu <85933175+tabby123-dev@users.noreply.github.com>
2026-02-22 16:07:33 +0300

    Change app.listen to use 0.0.0.0 and move port declaration

--- src/main.ts
+++ src/main.ts
@@ -39,7 +39,8 @@ async function bootstrap() {
   app.useGlobalInterceptors(new LoggingInterceptor(loggerService));
   app.useGlobalFilters(new HttpExceptionFilter());
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(port, '0.0.0.0');
+  const port = process.env.PORT ?? 3000;
   console.log(`Application is running on: ${await app.getUrl()}/${apiPrefix}`);
 }
 bootstrap();
