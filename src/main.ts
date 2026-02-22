33ca0d60237de83a14451bf6b1b6021d8552114a
Tabitha Ndungu <85933175+tabby123-dev@users.noreply.github.com>
2026-02-22 16:12:52 +0300

    Fix port variable declaration before app.listen

--- src/main.ts
+++ src/main.ts
@@ -39,8 +39,8 @@ async function bootstrap() {
   app.useGlobalInterceptors(new LoggingInterceptor(loggerService));
   app.useGlobalFilters(new HttpExceptionFilter());
 
-  await app.listen(port, '0.0.0.0');
   const port = process.env.PORT ?? 3000;
+  await app.listen(port, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}/${apiPrefix}`);
 }
 bootstrap();
